d271ed82052f01cb774262a37cc21b88b760b257
msyafiqq <muhammad.syafiq@geniebook.com>
2024-11-18 10:46:11 +0700

    lib: Add jmdns

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,6 +48,8 @@ dependencies {
     implementation(libs.androidx.lifecycle.viewmodel.ktx)
     implementation(libs.androidx.navigation.fragment.ktx)
     implementation(libs.androidx.navigation.ui.ktx)
+    implementation(files("libs/jmdns-3.5.1.jar"))
+    implementation("org.slf4j:slf4j-api:2.0.16")
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
